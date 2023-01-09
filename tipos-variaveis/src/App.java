1e9f1ff36ec29b9a93370dfdcc02182ffbbaa159
Wallace Medeiros <101136181+wallacem8@users.noreply.github.com>
2023-01-08 23:25:25 -0300

    comitando

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,16 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+
+        numero = 20;
+        System.out.println(numero);
+
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+        
     }
 }
